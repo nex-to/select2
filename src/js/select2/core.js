0a4676ba7525874d560be29ac745b694b04b136f
Julian Hirt <juleshirt@googlemail.com>
2016-04-26 17:59:08 +0200

    Add fix to keep select closed after unselecting option

--- src/js/select2/core.js
+++ src/js/select2/core.js
@@ -275,6 +275,12 @@ define([
 
     this.on('query', function (params) {
       if (!self.isOpen()) {
+        // key closed when removed item
+        if ($(window.event.target)
+          .hasClass('select2-selection__choice__remove')) {
+          return;
+        }
+        // end
         self.trigger('open', {});
       }
 
